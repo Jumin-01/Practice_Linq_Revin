4c8c823eb49c69dc1d2a63d5cfd4ad76104841f3
Jumin-01 <revin.veniamin2003@gmail.com>
2023-12-07 17:54:53 +0200

    Query1

--- Practice_Linq/Program.cs
+++ Practice_Linq/Program.cs
@@ -48,11 +48,15 @@ namespace Practice_Linq
         {
             //Query 1: Вивести всі матчі, які відбулися в Україні у 2012 році.
 
-            var selectedGames = games; // Корегуємо запит !!!
-
+            var selectedGames = games.Where(game => game.Country == "Ukraine" && game.Date.Year == 2012); ; // Корегуємо запит !!!
+           
 
             // Перевірка
             Console.WriteLine("\n======================== QUERY 1 ========================");
+            foreach (var item in selectedGames)
+            {
+                Console.WriteLine($"{item.Date.ToShortDateString()} {item.Home_team} - {item.Away_team}, Scope: {item.Home_score} - {item.Away_score}, Country: {item.Country}");
+            }
 
             // див. приклад як має бути виведено:
 
